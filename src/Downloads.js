5a87ec5da5f636d8609b06ab96f25bb51e6f89da
Philip Krück <philip.krueck@apploft.de>
2020-03-11 11:24:40 +0100

    added downloads js

---
+++ src/Downloads.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../node_modules/react-vis/dist/style.css';
+import {XYPlot, LineSeries, XAxis, YAxis, HorizontalGridLines, VerticalGridLines} from 'react-vis';
+
+const data = [
+    {x: 0, y: 8},
+    {x: 1, y: 5},
+    {x: 2, y: 4},
+    {x: 3, y: 9},
+    {x: 4, y: 1},
+    {x: 5, y: 7},
+    {x: 6, y: 6},
+    {x: 7, y: 3},
+    {x: 8, y: 2},
+    {x: 9, y: 0}
+  ];
+
+function Downloads() {
+    return (
+        <div>
+            <h1>Downloads</h1>    
+            <XYPlot height={300} width={300} >
+                <XAxis />
+                <YAxis />
+                <HorizontalGridLines />
+                <VerticalGridLines />
+                <LineSeries data={data}></LineSeries>
+
+            </XYPlot>
+        </div>
+    )
+}
+
+export default Downloads;
